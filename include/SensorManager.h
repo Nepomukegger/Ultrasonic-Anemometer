31884d499ca6c3e6143618d6ce409e754d4d32ca
Nepomuk <nepomukegger@gmail.com>
2024-11-20 13:20:44 +0100

    some changes to SensorManager.h and started implementing SensorManager.cpp

--- include/SensorManager.h
+++ include/SensorManager.h
@@ -1,9 +1,11 @@
+#pragma once
 #include "config.h"
 #include <esp_timer.h>
+#include <Arduino.h>
 
 
-struct Sensor{
-    int setup();
+struct UltrasonicSensor{
+    UltrasonicSensor();
     int64_t measureTime();
     bool outputPWM();
 };
