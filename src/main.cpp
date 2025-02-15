375c919e122aa23f3dfa12da8ed0a207f55aafea
der.hoecki <chrisi.hoeck@gmail.com>
2025-02-15 16:43:46 +0100

    Added PWM Signal for ESP32 with AnalogWrite

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <Arduino.h>
-#include <arduino-esp32/esp32-hal-ledc.h>
 
-const int ledPin = 1;
-const int freq = 305000;
-const int resolution = 4;
+const int ledPin = 1;           // LED Pin
+const int dutyCycle = 128;       // PWM Duty Cycle (0-255)
+const int freq = 305000;         // PWM Frequenz
+const int resolution = 4;        // PWM Auflösung (z. B. 4 Bits, 0-15)
+const int ledChannel = 0;        // PWM Kanal (0-15)
+const int timer = 0;             // Timer für den PWM Kanal (0-1)
 
 void setup() {
-// write your initialization code here
-ledcAttach(ledPin, freq, resolution);
+  // Konfiguriere den PWM-Kanal
+  analogWriteFrequency(freq);
 }
 
 void loop() {
-// write your code here
-ledcWrite(ledPin, 8);
-delay(100000);
+  // Setze den PWM-Wert (Duty Cycle) auf 8 (von 0 bis 15 bei einer Auflösung von 4 Bits)
+  analogWrite(ledPin, dutyCycle);
+  delay(1000); // Verzögerung von 1 Sekunde
 }
