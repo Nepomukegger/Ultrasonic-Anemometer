0028a0827cd31ef589018c8cec64d5659e6264e6
der.hoecki <chrisi.hoeck@gmail.com>
2025-02-15 16:09:44 +0100

    try to add PWM Signal for Measurments
    Co-authored-by: Nepomuk <Nepomukegger@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <Arduino.h>
+#include <arduino-esp32/esp32-hal-ledc.h>
+
+const int ledPin = 1;
+const int freq = 305000;
+const int resolution = 4;
+
 void setup() {
 // write your initialization code here
+ledcAttach(ledPin, freq, resolution);
 }
 
 void loop() {
 // write your code here
+ledcWrite(ledPin, 8);
+delay(100000);
 }
