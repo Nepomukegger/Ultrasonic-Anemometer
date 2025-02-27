a070d949ca388384ef0f377d68cf5d7d80dd9047
Nepomuk <nepomukegger@gmail.com>
2025-02-27 12:59:29 +0100

    program to detect the core it is running on

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,12 @@
 #include <Arduino.h>
 
-const int ledPin = 1;           // LED Pin
-const int dutyCycle = 128;       // PWM Duty Cycle (0-255)
-const int freq = 305000;         // PWM Frequenz
-const int resolution = 4;        // PWM Auflösung (z. B. 4 Bits, 0-15)
-const int ledChannel = 0;        // PWM Kanal (0-15)
-const int timer = 0;             // Timer für den PWM Kanal (0-1)
-
 void setup() {
-  // Konfiguriere den PWM-Kanal
-  analogWriteFrequency(freq);
+  Serial.begin(115200);
+  Serial.print("setup() running on core ");
+  Serial.println(xPortGetCoreID());
 }
 
 void loop() {
-  // Setze den PWM-Wert (Duty Cycle) auf 8 (von 0 bis 15 bei einer Auflösung von 4 Bits)
-  analogWrite(ledPin, dutyCycle);
-  delay(1000); // Verzögerung von 1 Sekunde
+  Serial.print("loop() running on core ");
+  Serial.println(xPortGetCoreID());
 }
