ef946c5973e3a09a7a5bd1349d4268841c0a4c78
Nepomuk <nepomukegger@gmail.com>
2025-02-27 13:33:55 +0100

    program to detect the core it is running on

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
 
+TaskHandle_t Task1;
+
+void task(void * pvParameters);
+
 void setup() {
   Serial.begin(115200);
-  Serial.print("setup() running on core ");
-  Serial.println(xPortGetCoreID());
+  pinMode(16, OUTPUT);
+  pinMode(19, OUTPUT);
+  Serial.print("core: ");
+  Serial.print(xPortGetCoreID());
+  Serial.print("Time");
+  Serial.println(esp_timer_get_time());
+  xTaskCreatePinnedToCore(
+                    task,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task1,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 0 */
 }
 
 void loop() {
-  Serial.print("loop() running on core ");
-  Serial.println(xPortGetCoreID());
+  Serial.print("core: ");
+  Serial.print(xPortGetCoreID());
+  Serial.print("Time");
+  Serial.println(esp_timer_get_time());
+  delay(10000);
+}
+
+void task(void *pvParameters) {
+  while (true){
+    Serial.print("core: ");
+    Serial.print(xPortGetCoreID());
+    Serial.print("Time");
+    Serial.println(esp_timer_get_time());
+    delay(10000);
+  }
 }
