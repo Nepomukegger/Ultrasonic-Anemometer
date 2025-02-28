61516d604d800e5acff4bcf0efbf3d1a675d0826
Nepomuk <nepomukegger@gmail.com>
2025-02-28 13:35:10 +0100

    implementing logic for measurment

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    pinMode(19, INPUT);
 }
 
 void loop() {
 // write your code here
+    Serial.println(analogRead(19));
+    delay(5000);
 }
